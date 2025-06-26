f84d1b0d1fc0eb022fffc0e25b4d1fcf50b9fdf1
Puvitha-Git <puvithaganapathi@gmail.com>
2025-06-26 12:44:41 +0530

    third commit

--- src/pages/AddStudentPage.jsx
+++ src/pages/AddStudentPage.jsx
@@ -71,7 +71,7 @@ const AddStudentPage = () => {
       console.log('Main Domain:', domain);
       console.log('Specific Domain:', specificDomain);
       alert("Certificate ready to download!");
-      window.location.reload();
+      
     }
   };
 
